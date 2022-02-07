d6da7901a3a8256b1ef30fb2cc3fa01aaf4a5531
Stefan Green <grinchuk.stepan@gmail.com>
2022-02-07 09:23:55 +0400

    1

--- EstiManager_2/Zip.Designer.cs
+++ EstiManager_2/Zip.Designer.cs
@@ -58,11 +58,11 @@ namespace EstiManager_2
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(105, 30);
+            this.label1.Location = new System.Drawing.Point(106, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(287, 25);
+            this.label1.Size = new System.Drawing.Size(272, 25);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Создать резервныую копию?";
+            this.label1.Text = "Создать резервную копию?";
             // 
             // ZIp
             // 
